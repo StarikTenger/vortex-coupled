63feac7aba77528fa79bc905868a01a6a67f1bb0
Andrei Ilin <stariktenger@gmail.com>
2026-06-10 13:39:59 +0200

    run-tests.sh -- add no debug case

--- util/run-tests.sh
+++ util/run-tests.sh
@@ -7,10 +7,30 @@ RED='\033[0;31m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
+echo "debug=0:"
+
 for app in "${apps[@]}"; do
     logfile="blackbox_logs/${app}.log"
 
     ./ci/blackbox.sh --cores=4 --app="$app" --debug=0 >"$logfile" 2>&1
+    status=$?
+
+    if [ $status -eq 0 ]; then
+        echo -e "${GREEN}[PASS]${NC}  $app"
+    elif [ $status -gt 128 ]; then
+        signal=$((status - 128))
+        echo -e "${YELLOW}[CRASH]${NC} $app (terminated by signal $signal)"
+    else
+        echo -e "${RED}[FAIL]${NC}  $app (exit code $status)"
+    fi
+done
+
+echo "no debug flag:"
+
+for app in "${apps[@]}"; do
+    logfile="blackbox_logs/${app}.log"
+
+    ./ci/blackbox.sh --cores=4 --app="$app" >"$logfile" 2>&1
     status=$?
 
     if [ $status -eq 0 ]; then
